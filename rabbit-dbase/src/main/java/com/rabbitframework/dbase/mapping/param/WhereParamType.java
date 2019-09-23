5ac9730ead0307011b5d410c7528e9d29b6d8fbf
justin <28554768@qq.com>
2019-09-23 17:47:35 +0800

    修改

--- rabbit-dbase/src/main/java/com/rabbitframework/dbase/mapping/param/WhereParamType.java
+++ rabbit-dbase/src/main/java/com/rabbitframework/dbase/mapping/param/WhereParamType.java
@@ -5,89 +5,105 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.rabbitframework.commons.utils.StringUtils;
+
 /**
  * 数据库参数组合实例类 提供对数据库条件解析的接口 oredCriteria条件集体合，一般情况下使用createCriteria()方法
  * 特殊情况下，调用createCriteria(criteria)或addCreateCriteria()方法
  */
 public class WhereParamType {
 
-    protected List<Criteria> oredCriteria;
-    protected Map<String, Object> params;
-    protected String orderby = null;
+	protected List<Criteria> oredCriteria;
+	protected Map<String, Object> params;
+	protected String orderby = null;
+	protected String defineCondition = null;
 
-    public WhereParamType() {
-        oredCriteria = new ArrayList<Criteria>();
-        params = new HashMap<String, Object>();
-    }
-    public boolean isParamsValid() {
-        return params.size() > 0;
-    }
+	public WhereParamType() {
+		oredCriteria = new ArrayList<Criteria>();
+		params = new HashMap<String, Object>();
+	}
 
-    public List<Criteria> getOredCriteria() {
-        return oredCriteria;
-    }
+	public boolean isDefCondition() {
+		return StringUtils.isNotBlank(defineCondition);
+	}
 
-    public Map<String, Object> getParams() {
-        return params;
-    }
+	public boolean isParamsValid() {
+		return params.size() > 0;
+	}
 
-    public void setParams(Map<String, Object> params) {
-        if (params != null && params.size() > 0) {
-            this.params.putAll(params);
-        }
-    }
+	public List<Criteria> getOredCriteria() {
+		return oredCriteria;
+	}
 
-    public void put(String key, Object value) {
-        params.put(key, value);
-    }
+	public Map<String, Object> getParams() {
+		return params;
+	}
 
-    /**
-     * 根据参数，增加条件集体合
-     *
-     * @param criteria
-     */
-    public void createCriteria(Criteria criteria) {
-        oredCriteria.add(criteria);
-    }
+	public void setParams(Map<String, Object> params) {
+		if (params != null && params.size() > 0) {
+			this.params.putAll(params);
+		}
+	}
 
-    /**
-     * 实例化条件类，增加条件集合
-     *
-     * @return
-     */
-    public Criteria addCreateCriteria() {
-        Criteria criteria = createCriteriaInternal();
-        oredCriteria.add(criteria);
-        return criteria;
-    }
+	public void put(String key, Object value) {
+		params.put(key, value);
+	}
 
-    /**
-     * 限定条件集体合，size==1
-     *
-     * @return
-     */
-    public Criteria createCriteria() {
-        Criteria criteria = createCriteriaInternal();
-        if (oredCriteria.size() == 0) {
-            oredCriteria.add(criteria);
-        }
-        return criteria;
-    }
+	/**
+	 * 根据参数，增加条件集体合
+	 *
+	 * @param criteria
+	 */
+	public void createCriteria(Criteria criteria) {
+		oredCriteria.add(criteria);
+	}
 
-    protected Criteria createCriteriaInternal() {
-        Criteria criteria = new Criteria();
-        return criteria;
-    }
+	/**
+	 * 实例化条件类，增加条件集合
+	 *
+	 * @return
+	 */
+	public Criteria addCreateCriteria() {
+		Criteria criteria = createCriteriaInternal();
+		oredCriteria.add(criteria);
+		return criteria;
+	}
 
-    public void clear() {
-        oredCriteria.clear();
-    }
+	/**
+	 * 限定条件集体合，size==1
+	 *
+	 * @return
+	 */
+	public Criteria createCriteria() {
+		Criteria criteria = createCriteriaInternal();
+		if (oredCriteria.size() == 0) {
+			oredCriteria.add(criteria);
+		}
+		return criteria;
+	}
 
-    public void setOrderby(String orderby) {
-        this.orderby = orderby;
-    }
+	protected Criteria createCriteriaInternal() {
+		Criteria criteria = new Criteria();
+		return criteria;
+	}
 
-    public String getOrderby() {
-        return orderby;
-    }
+	public void clear() {
+		oredCriteria.clear();
+	}
+
+	public void setOrderby(String orderby) {
+		this.orderby = orderby;
+	}
+
+	public String getOrderby() {
+		return orderby;
+	}
+
+	public void setDefineCondition(String defineCondition) {
+		this.defineCondition = defineCondition;
+	}
+
+	public String getDefineCondition() {
+		return defineCondition;
+	}
 }
