6e3ed8ebfd6891235ebcd9b753c39fb40e0d8889
liangjy <28554768@qq.com>
2019-11-04 00:27:46 +0800

    update

--- rabbit-jade-pom/rabbit-jade/src/main/java/com/rabbitframework/jade/dataaccess/datasource/RandomDataSourceFactory.java
+++ rabbit-jade-pom/rabbit-jade/src/main/java/com/rabbitframework/jade/dataaccess/datasource/RandomDataSourceFactory.java
@@ -13,35 +13,36 @@ import java.util.Random;
 import javax.sql.DataSource;
 
 public class RandomDataSourceFactory implements DataSourceFactory {
-	private Random random = new Random();
-	private List<DataSourceBean> dataSources = Collections.emptyList();
+    private Random random = new Random();
+    private List<DataSourceBean> dataSources = Collections.emptyList();
 
-	@Override
-	public void addDataSource(String key, DataSourceBean dataSourceBean) {
-		if (dataSourceBean == null) {
-			throw new NullPointerException("dataSourceBean is null");
-		}
-		if (dataSources.size() == 0) {
-			dataSources = new ArrayList<DataSourceBean>(dataSources);
-		}
-	}
+    @Override
+    public void addDataSource(String key, DataSourceBean dataSourceBean) {
+        if (dataSourceBean == null) {
+            throw new NullPointerException("dataSourceBean is null");
+        }
+        if (this.dataSources.size() == 0) {
+            this.dataSources = new ArrayList<DataSourceBean>(dataSources);
+        }
+        this.dataSources.add(dataSourceBean);
+    }
 
-	@Override
-	public DataSource getDataSource(MappedStatement mappedStatement) {
-		if (dataSources.size() == 0) {
-			return null;
-		}
-		int index = random.nextInt(dataSources.size()); // 0.. size
-		return dataSources.get(index).getDataSource();
-	}
+    @Override
+    public DataSource getDataSource(MappedStatement mappedStatement) {
+        if (dataSources.size() == 0) {
+            throw new NullPointerException("dataSources is null");
+        }
+        int index = random.nextInt(dataSources.size()); // 0.. size
+        return dataSources.get(index).getDataSource();
+    }
 
-	@Override
-	public Dialect getDialect(MappedStatement mappedStatement) {
-		if (dataSources.size() == 0) {
-			return null;
-		}
-		int index = random.nextInt(dataSources.size()); // 0.. size
-		String dialectStr = dataSources.get(index).getDialect();
-		return DefaultDialect.newInstances(dialectStr);
-	}
+    @Override
+    public Dialect getDialect(MappedStatement mappedStatement) {
+        if (dataSources.size() == 0) {
+            throw new NullPointerException("dataSources is null");
+        }
+        int index = random.nextInt(dataSources.size()); // 0.. size
+        String dialectStr = dataSources.get(index).getDialect();
+        return DefaultDialect.newInstances(dialectStr);
+    }
 }
