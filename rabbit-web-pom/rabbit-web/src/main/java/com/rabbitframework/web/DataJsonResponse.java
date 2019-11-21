42f1c2dafe47facc83e27fd2dd1d20ba4d38ae5d
justin <28554768@qq.com>
2019-11-22 00:59:38 +0800

    update

--- rabbit-web-pom/rabbit-web/src/main/java/com/rabbitframework/web/DataJsonResponse.java
+++ rabbit-web-pom/rabbit-web/src/main/java/com/rabbitframework/web/DataJsonResponse.java
@@ -8,79 +8,79 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.rabbitframework.commons.utils.StatusCode;
 
 public class DataJsonResponse {
-    private Map<String, Object> json = new HashMap<String, Object>();
+	private Map<String, Object> json = new HashMap<String, Object>();
 
-    private Map<String, Object> data;
+	private Map<String, Object> data;
 
-    public DataJsonResponse set(String key, Object value) {
-        json.put(key, value);
-        return this;
-    }
+	public DataJsonResponse set(String key, Object value) {
+		json.put(key, value);
+		return this;
+	}
 
-    public DataJsonResponse setData(String key, Object value) {
-        if (data == null) {
-            data = new HashMap<String, Object>();
-        }
-        data.put(key, value);
-        return this;
-    }
+	public DataJsonResponse setData(String key, Object value) {
+		if (data == null) {
+			data = new HashMap<String, Object>();
+		}
+		data.put(key, value);
+		return this;
+	}
 
-    public DataJsonResponse setData(Object value) {
-        json.put("data", value);
-        return this;
-    }
+	public DataJsonResponse setData(Object value) {
+		json.put("data", value);
+		return this;
+	}
 
-    public DataJsonResponse setMessage(String success) {
-        json.put("message", success);
-        return this;
-    }
+	public DataJsonResponse setMessage(String success) {
+		json.put("message", success);
+		return this;
+	}
 
-    public DataJsonResponse setStatus(boolean status, String message) {
-        setStatus(status);
-        json.put("message", message);
-        return this;
-    }
+	public DataJsonResponse setStatus(boolean status, String message) {
+		setStatus(status);
+		json.put("message", message);
+		return this;
+	}
 
-    public DataJsonResponse setStatus(boolean status) {
-        json.put("status", status ? StatusCode.SC_OK : StatusCode.FAIL);
-        return this;
-    }
+	public DataJsonResponse setStatus(boolean status) {
+		json.put("status", status ? StatusCode.SC_OK : StatusCode.FAIL);
+		return this;
+	}
 
-    public DataJsonResponse setStatus(int status) {
-        json.put("status", status);
-        return this;
-    }
+	public DataJsonResponse setStatus(int status) {
+		json.put("status", status);
+		return this;
+	}
 
-    public DataJsonResponse setStatus(int status, String message) {
-        json.put("status", status);
-        json.put("message", message);
-        return this;
-    }
+	public DataJsonResponse setStatus(int status, String message) {
+		json.put("status", status);
+		json.put("message", message);
+		return this;
+	}
 
-    /**
-     * 空值自动转换
-     *
-     * @return
-     */
-    public String toJson() {
-        if (data != null && !json.containsKey("data")) {
-            json.put("data", data);
-        }
-        return JSON.toJSONString(json, SerializerFeature.BrowserCompatible, SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteNullNumberAsZero, SerializerFeature.WriteNullNumberAsZero,
-                SerializerFeature.WriteNullListAsEmpty, SerializerFeature.WriteNullStringAsEmpty,
-                SerializerFeature.WriteNullBooleanAsFalse);
-    }
+	/**
+	 * 空值自动转换相匹配的类型属性
+	 *
+	 * @return
+	 */
+	public String toJson() {
+		if (data != null && !json.containsKey("data")) {
+			json.put("data", data);
+		}
+		return JSON.toJSONString(json, SerializerFeature.BrowserCompatible, SerializerFeature.WriteMapNullValue,
+				SerializerFeature.WriteNullNumberAsZero, SerializerFeature.WriteNullNumberAsZero,
+				SerializerFeature.WriteNullListAsEmpty, SerializerFeature.WriteNullStringAsEmpty,
+				SerializerFeature.WriteNullBooleanAsFalse, SerializerFeature.SkipTransientField);
+	}
 
-    /**
-     * 忽略空值
-     *
-     * @return
-     */
-    public String toJsonNoNull() {
-        if (data != null && !json.containsKey("data")) {
-            json.put("data", data);
-        }
-        return JSON.toJSONString(json, SerializerFeature.BrowserCompatible);
-    }
+	/**
+	 * 空值自动过滤，不显示
+	 *
+	 * @return
+	 */
+	public String toJsonNoNull() {
+		if (data != null && !json.containsKey("data")) {
+			json.put("data", data);
+		}
+		return JSON.toJSONString(json, SerializerFeature.BrowserCompatible, SerializerFeature.SkipTransientField);
+	}
 }
