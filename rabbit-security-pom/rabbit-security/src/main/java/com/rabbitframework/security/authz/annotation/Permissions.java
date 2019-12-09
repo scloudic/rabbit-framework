64f5cf04d2b09d98b322085243480ec2d192253f
justin <28554768@qq.com>
2019-12-09 23:51:17 +0800

    update

--- rabbit-security-pom/rabbit-security/src/main/java/com/rabbitframework/security/authz/annotation/Permissions.java
+++ rabbit-security-pom/rabbit-security/src/main/java/com/rabbitframework/security/authz/annotation/Permissions.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 权限拦截注解
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Permissions {
--- rabbit-security-pom/rabbit-security/src/main/java/com/rabbitframework/security/authz/annotation/Roles.java
+++ rabbit-security-pom/rabbit-security/src/main/java/com/rabbitframework/security/authz/annotation/Roles.java
@@ -7,20 +7,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 权色拦截注解
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Roles {
 
-    /**
-     * A single String role name or multiple comma-delimitted role names required in order for the method
-     * invocation to be allowed.
-     */
     String[] value();
 
-    /**
-     * The logical operation for the permission check in case multiple roles are specified. AND is the default
-     *
-     * @since 1.1.0
-     */
     Logical logical() default Logical.AND;
 }
