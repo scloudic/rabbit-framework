7b44b19070821e79b76a454c1ec7f123293e6095
justin <28554768@qq.com>
2020-02-29 19:03:32 +0800

    update

--- rabbit-web-pom/rabbit-web/src/main/java/com/rabbitframework/web/AbstractContextResource.java
+++ rabbit-web-pom/rabbit-web/src/main/java/com/rabbitframework/web/AbstractContextResource.java
@@ -108,4 +108,33 @@ public abstract class AbstractContextResource extends RabbitContextResource {
         String value = dataJsonResponse.toJson(isNullToEmpty);
         return ResponseUtils.ok(value);
     }
+
+    /**
+     * 获取ip地址
+     *
+     * @param request
+     * @return
+     */
+    public String getIpAddr(HttpServletRequest request) {
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ipAddress != null && ipAddress.length() > 15) { //"***.***.***.***".length() = 15
+            if (ipAddress.indexOf(",") > 0) {
+                ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+            }
+        }
+        return ipAddress;
+    }
+
 }
