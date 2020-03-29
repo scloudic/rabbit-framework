72b9b1027a3f1bc7336cc408749a5ead4d245278
liangjy <28554768@qq.com>
2020-03-29 13:49:23 +0800

    update

--- rabbit-security-pom/rabbit-security/src/main/java/com/rabbitframework/security/SecurityUser.java
+++ rabbit-security-pom/rabbit-security/src/main/java/com/rabbitframework/security/SecurityUser.java
@@ -1,68 +1,99 @@
 package com.rabbitframework.security;
 
+import com.tjzq.commons.utils.StringUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class SecurityUser implements java.io.Serializable {
-	private static final long serialVersionUID = 1L;
-	private String userName;
-	private String userId;
-	private String loginName;
-	private Map<String, Object> other;
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String userName;
+    private String realName;
+    private String userId;
+    private String loginName;
+    private Map<String, Object> other;
 
-	public SecurityUser() {
+    public SecurityUser() {
 
-	}
+    }
 
-	public void addOther(String key, Object value) {
-		if (other == null) {
-			other = new HashMap<String, Object>();
-		}
-		other.put(key, value);
-	}
+    public void addOther(String key, Object value) {
+        if (other == null) {
+            other = new HashMap<String, Object>();
+        }
+        other.put(key, value);
+    }
 
-	public void setOther(Map<String, Object> other) {
-		this.other = other;
-	}
+    public void setOther(Map<String, Object> other) {
+        this.other = other;
+    }
 
-	public Map<String, Object> getOther() {
-		if (other == null) {
-			return new HashMap<String, Object>();
-		}
-		return other;
-	}
+    public Map<String, Object> getOther() {
+        if (other == null) {
+            return new HashMap<String, Object>();
+        }
+        return other;
+    }
 
-	public SecurityUser(String userId, String loginName) {
-		this.userId = userId;
-		this.loginName = loginName;
-	}
+    public SecurityUser(String userId, String loginName) {
+        this(userId, userId, loginName);
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public SecurityUser(String id, String userId, String loginName) {
+        this.id = id;
+        this.userId = userId;
+        this.loginName = loginName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getUserId() {
-		return userId;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public String getLoginName() {
-		return loginName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public void setLoginName(String loginName) {
-		this.loginName = loginName;
-	}
+    public String getUserId() {
+        return userId;
+    }
 
-	@Override
-	public String toString() {
-		return userId;
-	}
+    public void setUserId(String userId) {
+        if (StringUtils.isBlank(id)) {
+            this.id = userId;
+        }
+        this.userId = userId;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    @Override
+    public String toString() {
+        if (StringUtils.isBlank(id)) {
+            return userId;
+        }
+        return id;
+    }
 }
