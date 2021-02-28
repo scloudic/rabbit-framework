33435b38143e78a43ec5beaf19c2dde6414340cd
liangjy <28554768@qq.com>
2021-02-28 10:44:55 +0800

    update

--- rabbit-security-pom/rabbit-security-spring-boot-starter/src/main/java/com/rabbitframework/security/springboot/configure/LifecycleAutoConfiguration.java
+++ rabbit-security-pom/rabbit-security-spring-boot-starter/src/main/java/com/rabbitframework/security/springboot/configure/LifecycleAutoConfiguration.java
@@ -2,12 +2,16 @@ package com.rabbitframework.security.springboot.configure;
 
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Role;
+import org.springframework.scheduling.config.TaskManagementConfigUtils;
 
 @Configuration
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class LifecycleAutoConfiguration {
     @Bean(name = "lifecycleBeanPostProcessor")
     @ConditionalOnMissingBean
