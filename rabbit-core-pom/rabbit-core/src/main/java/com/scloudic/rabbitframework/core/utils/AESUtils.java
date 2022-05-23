96793c534b9283a0ec88115d85bf86c6502645df
liangjy <28554768@qq.com>
2022-05-23 14:48:30 +0800

    update

--- rabbit-core-pom/rabbit-core/src/main/java/com/scloudic/rabbitframework/core/utils/AESUtils.java
+++ rabbit-core-pom/rabbit-core/src/main/java/com/scloudic/rabbitframework/core/utils/AESUtils.java
@@ -17,13 +17,13 @@ public class AESUtils {
     private static final String AES_TYPE = "AES/ECB/PKCS5Padding";
 
     /**
-     * aes加密,返回base64
+     * aes加密,返回base64编码后的字符串
      *
      * @param secretKey
      * @param content
      * @return
      */
-    public static String encryptToBase64(String secretKey, String content) {
+    public static String encrypt(String secretKey, String content) {
         try {
             SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
             Cipher cipher = Cipher.getInstance(AES_TYPE);
@@ -36,25 +36,6 @@ public class AESUtils {
         }
     }
 
-    public static String encryptToBase64(String secretKey, String content, byte[] ivByte) {
-        try {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            if (ivByte == null) {
-                cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            } else {
-                IvParameterSpec iv = new IvParameterSpec(ivByte);
-                cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, iv);
-            }
-            byte[] encryptedData = cipher.doFinal(content.getBytes(CODE_TYPE));
-            return Base64Utils.encodeToString(encryptedData);
-        } catch (Exception e) {
-            log.error(String.format("加密失败, key = %s , data = %s " + e, secretKey, content));
-            throw new CodecException(e.getMessage(), e);
-        }
-    }
-
-
     /**
      * 将base64位内容进行解密
      *
@@ -62,29 +43,14 @@ public class AESUtils {
      * @param base64Encrypt
      * @return
      */
-    public static String decryptFromBase64(String secretKey, String base64Encrypt) {
+    public static String decrypt(String secretKey, String base64Encrypt) {
         try {
-            byte[] byteMi = Base64Utils.decode(base64Encrypt);
+            byte[] byteContent = Base64Utils.decode(base64Encrypt);
             SecretKeySpec secretKeySpec = new SecretKeySpec(
                     secretKey.getBytes(), "AES");
             Cipher cipher = Cipher.getInstance(AES_TYPE);
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-            byte[] decryptedData = cipher.doFinal(byteMi);
-            return new String(decryptedData, CODE_TYPE);
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            throw new CodecException(e.getMessage(), e);
-        }
-    }
-
-    public static String decryptFromBase64(String secretKey, String base64Encrypt, byte[] ivByte) {
-        try {
-            byte[] byteMi = Base64Utils.decode(base64Encrypt);
-            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            IvParameterSpec iv = new IvParameterSpec(ivByte);
-            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, iv);
-            byte[] decryptedData = cipher.doFinal(byteMi);
+            byte[] decryptedData = cipher.doFinal(byteContent);
             return new String(decryptedData, CODE_TYPE);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
