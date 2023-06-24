290ad5177504978d2c65ec50e26c1cf01f24df46
liangjy <28554768@qq.com>
2023-06-24 16:54:25 +0800

    修改自定义SQL

--- rabbit-jbatis-pom/rabbit-jbatis/src/main/java/com/scloudic/rabbitframework/jbatis/scripting/sql/CustomerSelect.java
+++ rabbit-jbatis-pom/rabbit-jbatis/src/main/java/com/scloudic/rabbitframework/jbatis/scripting/sql/CustomerSelect.java
@@ -1,7 +1,13 @@
 package com.scloudic.rabbitframework.jbatis.scripting.sql;
 
+import com.scloudic.rabbitframework.core.propertytoken.PropertyParser;
 import com.scloudic.rabbitframework.jbatis.mapping.SqlCommendType;
 import com.scloudic.rabbitframework.jbatis.mapping.param.Where;
+
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerSelect extends BaseSQLParser {
     @Override
@@ -11,7 +17,19 @@ public class CustomerSelect extends BaseSQLParser {
         }
         String sqlValue = "";
         String where = getSearchSql();
-        sqlValue = sqlScript + " " + where + " ";
+        Pattern pattern = Pattern.compile("\\$\\$\\{(.*?)\\}");
+        Matcher matcher = pattern.matcher(sqlScript);
+        ArrayList<String> values = new ArrayList<String>();
+        while (matcher.find()) {
+            values.add(matcher.group(1));
+        }
+        if (values.size() > 0) {
+            Properties properties = new Properties();
+            properties.put(values.get(0), where);
+            sqlValue = PropertyParser.parseOther("$${", "}", sqlScript, properties);
+        } else {
+            sqlValue = sqlScript + " " + where + " ";
+        }
         StringBuilder sb = new StringBuilder();
         sb.append("<if test=\"defCondition\" > ${defineCondition} </if>");
         sb.append("<if test=\"groupBy != null\" > group by ${groupBy} </if>");
